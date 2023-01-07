94e5f20a381931dcdfe3212dc2286040485e4045
k4ik <121681971+k4ik@users.noreply.github.com>
2023-01-07 07:36:04 -0300

    if

---
+++ curso/bin/aula3.dart
@@ -0,0 +1,12 @@
+void main() {
+  //       Estruturas de fluxo
+
+  //  If
+
+  int idade = 22;
+  if (idade >= 18) {
+    print("Apto a tirar carteira!");
+  } else {
+    print("Inapto a tirar carteira!");
+  }
+}
