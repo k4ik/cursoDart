ac80e3a3551dd6759f7ad118bf5cf03527247545
k4ik <121681971+k4ik@users.noreply.github.com>
2023-01-07 07:46:15 -0300

    switch

--- curso/bin/aula3.dart
+++ curso/bin/aula3.dart
@@ -9,4 +9,33 @@ void main() {
   } else {
     print("Inapto a tirar carteira!");
   }
+
+  //  Switch
+
+  int valorPremiado = 7;
+  switch (valorPremiado) {
+    case 1:
+      print("Errooouu");
+      break;
+    case 2:
+      print("Nope");
+      break;
+    case 3:
+      print("Da 0 pra ele");
+      break;
+    case 4:
+      print("Quase");
+      break;
+    case 5:
+      print("NOO");
+      break;
+    case 6:
+      print("não");
+      break;
+    case 7:
+      print("ACERTOU AEEEE");
+      break;
+    default:
+      print("nope");
+  }
 }
