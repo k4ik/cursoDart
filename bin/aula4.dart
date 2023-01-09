16f6194bc7c83361526164fcc7217b07870bbefd
k4ik <121681971+k4ik@users.noreply.github.com>
2023-01-09 11:05:10 -0300

    aula 04

---
+++ bin/aula4.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Estruturas de Repetição
+
+  // for
+  for (int i = 1; i <= 10; i++) {
+    print(i * 2);
+  }
+
+
+  // while
+  int cont = 1;
+  while (cont != 20) {
+    print(cont);
+    cont++;
+  }
+}
--- test/curso_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:curso/curso.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
